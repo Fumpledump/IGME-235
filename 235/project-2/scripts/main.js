66622fb08beda72bb9e56195906d7df145d4ac60
Mr.Fumpledump <Fumpledump@gmail.com>
2023-04-24 21:59:05 -0400

    Project 2 complete

--- 235/project-2/scripts/main.js
+++ 235/project-2/scripts/main.js
@@ -7,6 +7,27 @@ const _checkButton = document.getElementById('check-answer');
 const _nextButton = document.getElementById('next-question');
 const _tryAgainButton = document.getElementById('try-again');
 const _result = document.getElementById('result');
+const _startScreen = document.getElementById('start-screen');
+const _startButton = document.getElementById('start-button');
+const _restartButton = document.getElementById('restart');
+const _gameContainer = document.querySelector('.game-container');
+const _gameBody = document.querySelector('.game-body');
+const _loadingScreen = document.getElementById('loading-screen');
+
+
+function startGame()
+{
+    _startScreen.style.display = "none";
+    _gameContainer.style.display = "block";
+    loadQuestion();
+}
+
+function restart()
+{
+    _startScreen.style.display = "flex";
+    _gameContainer.style.display = "none";
+}
+
 
 let correctAnswer = "";
 let score = 0;
@@ -14,9 +35,11 @@ let score = 0;
 // Event Listeners
 function eventListeners()
 {
+    _startButton.addEventListener('click', startGame);
     _checkButton.addEventListener('click', checkAnswer);
     _nextButton.addEventListener('click', loadQuestion);
     _tryAgainButton.addEventListener('click', loadQuestion);
+    _restartButton.addEventListener('click', restart);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -25,24 +48,69 @@ document.addEventListener('DOMContentLoaded', () => {
     _totalScore.innerHTML = `Total Score: ${score}`;
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    eventListeners();
+});
+
+let anyCategory = true;
+let anyDifficulty = true;
+let anyType = true;
+
 async function loadQuestion()
 {
-    const apiURL = 'https://opentdb.com/api.php?amount=1';
+    _gameBody.style.display = "none";
+    _loadingScreen.style.display = "block";
+
+    // Difficutly Setting
+    const difficulty = document.getElementById('difficulty-select').value;
+    let difficultySetting = "";
+    if(difficulty != "any")
+    {
+        difficultySetting = `&difficulty=${difficulty}`;
+        anyDifficulty = false;
+    }
+
+    // Category Setting
+    const category = document.getElementById('category-select').value;
+    let categorySetting = "";
+    categoryChoice = category;
+    if(category != "any")
+    {
+        categorySetting = `&category=${category}`;
+        anyCategory = false;
+    }
+
+    // Category Setting
+    const type = document.getElementById('type-select').value;
+    let typeSetting = "";
+    if(type != "any")
+    {
+        typeSetting = `&type=${type}`;
+        anyType = false;
+    }
+
+    const apiURL = `https://opentdb.com/api.php?amount=1${categorySetting}${difficultySetting}${typeSetting}`;
     const result = await fetch(`${apiURL}`);
     const data = await result.json();
     //console.log(data.results[0]);
+
+    console.log(apiURL);
 
     _result.innerHTML = "";
     _totalScore.innerHTML = `Total Score: ${score}`;
     showQuestion(data.results[0]);
     _nextButton.style.display = "none";
     _tryAgainButton.style.display = "none";
+    _restartButton.style.display = "none";
+    _gameBody.style.display = "block";
+    _loadingScreen.style.display = "none";
 }
 
 // Display Questions and Answers
 function showQuestion(data)
 {
     _checkButton.disabled = false;
+    _checkButton.style.display = "block";
     correctAnswer = data.correct_answer;
     let incorrectAnswers = data.incorrect_answers;
     let possibleAnswers = incorrectAnswers;
@@ -50,7 +118,21 @@ function showQuestion(data)
     // Insert Answers into Game with Correct One in Random Location
     possibleAnswers.splice(Math.floor(Math.random() * (incorrectAnswers.length + 1)), 0 , correctAnswer);
 
-    _question.innerHTML = `${data.question} <br> <span class = "category">${data.category} </span>`;
+    let currentSettings = "";
+    if(!anyCategory)
+    {
+        currentSettings += `<span class = "category">${data.category} </span>`;
+    }
+    if(!anyDifficulty)
+    {
+        currentSettings += `<span class = "category">${data.difficulty} </span>`;
+    }
+    if(!anyType)
+    {
+        currentSettings += `<span class = "category">${data.type} </span>`;
+    }
+
+    _question.innerHTML = `${data.question} <br> ${currentSettings}`;
 
     _options.innerHTML = `
         ${possibleAnswers.map((option, index) => `
@@ -79,9 +161,10 @@ function selectOption()
 // Answer Checking
 function checkAnswer()
 {
-    _checkButton.disabled = true;
     if(_options.querySelector('.selected'))
     {
+        _checkButton.style.display = "none";
+
         let selectedAnswer = _options.querySelector('.selected span').textContent.replace(/\s/g, '');
         let answer = correctAnswer.replace(/\s/g, '');
         console.log(selectedAnswer);
@@ -98,6 +181,7 @@ function checkAnswer()
             _result.innerHTML = `<p>Wrong Answer! <b>Correct Answer: ${correctAnswer}</b></p>`;
             score = 0;
             _tryAgainButton.style.display = "block";
+            _restartButton.style.display = "block";
         }
 
     }
